46eb5e42eb24d105460a0d8b14e004eb59ab7d27
vadimgerij8-droid <vadimgerij8@gmail.com>
2026-05-19 18:43:32 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,24 +1,24 @@
 String.prototype.hashCode = function () {
-  let hash = 0;
-  for (let i = 0; i < this.length; i++) {
-    hash = ((hash << 5) - hash) + this.charCodeAt(i);
-    hash |= 0;
-  }
-  return Math.abs(hash);
+    let hash = 0;
+    for (let i = 0; i < this.length; i++) {
+        hash = ((hash << 5) - hash) + this.charCodeAt(i);
+        hash |= 0;
+    }
+    return Math.abs(hash);
 };
 
-export function safeQuery(selector, parent = document) {
-  try { return parent.querySelector(selector); } catch (e) { return null; }
+function safeQuery(selector, parent = document) {
+    try { return parent.querySelector(selector); } catch (e) { return null; }
 }
 
-export function safeQueryAll(selector, parent = document) {
-  try { return Array.from(parent.querySelectorAll(selector)); } catch (e) { return []; }
+function safeQueryAll(selector, parent = document) {
+    try { return Array.from(parent.querySelectorAll(selector)); } catch (e) { return []; }
 }
 
-export function debounce(fn, delay) {
-  let timer;
-  return function (...args) {
-    clearTimeout(timer);
-    timer = setTimeout(() => fn.apply(this, args), delay);
-  };
+function debounce(fn, delay) {
+    let timer;
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn.apply(this, args), delay);
+    };
 }
